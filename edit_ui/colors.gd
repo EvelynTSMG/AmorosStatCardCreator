da0b477b164b9f9b08ed8dc498df5e907d210b2b
JesseOpenICT <jvdveen2004@gmail.com>
2026-04-21 13:04:42 +0200

    Patches
    - Fixed import numbers by forcing types. In fact most things are now typed in the creator. But importantly the stats now no longer import as floats because they're always ints.
    - Re-bound button commands from a process function to a more approprita unhandled input function.
    - CTRL + V now pasted your current clipboard image as an avatar
    - Added a label for recommended portrait size

--- edit_ui/colors.gd
+++ edit_ui/colors.gd
@@ -4,5 +4,5 @@ extends ColorPickerButton
 @export var value_connect : String
 
 
-func _on_color_changed(color: Color) -> void:
-	get_tree().current_scene.set(value_connect, color)
+func _on_color_changed(colorpicked: Color) -> void:
+	get_tree().current_scene.set(value_connect, colorpicked)
